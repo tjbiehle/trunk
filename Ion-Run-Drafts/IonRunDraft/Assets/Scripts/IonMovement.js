194e41e19dfaf692bd67ee9de208fd16ab495c67
urianawu <urianayou@gmail.com>
2014-09-29 13:42:41 -0500

    Ion Run First Draft
    You Wu - Draggable Resistor(Capsule), Drag Script, iTween
    path-following Script, Lighting Effect Objects. Working web player on
    facebook post. (Mistakenly spell Ion as Iron..)

---
+++ Ion-Run-Drafts/IonRunDraft/Assets/Scripts/IonMovement.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+var path = [Vector3(0,0,0)];
+var target = [Vector3(0,0,0)];
+var time = 5;
+
+function Start () {
+
+}
+
+function Update () {
+	iTween.MoveTo(gameObject, iTween.Hash("path",path, "time",time, "easetype", "linear"));
+
+}
---
+++ Ion-Run-Drafts/IonRunDraft/Assets/Scripts/MonsterMovement.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var path = [Vector3(0,0,0)];//, new Vector3(-3,0.5,4), new Vector3(3,0.5,4), new Vector3(3,0.5,-4)];//, new Vector3(-3,0.5, -4)];
+var target = [Vector3(0,0,0)];
+//var startPosition = ameObject.transform.position;
+var time = 5;
+
+function Start () {
+
+	
+}
+
+function Update () {
+	iTween.MoveTo(gameObject, iTween.Hash("path",path, "time",time, "easetype", "linear"));
+	//iTween.PutOnPath(this.gameObject, new Vector3[]{new Vector3(-3,0.5,4), new Vector3(-3,0.5,4)}, percent);
+	
+
+}
